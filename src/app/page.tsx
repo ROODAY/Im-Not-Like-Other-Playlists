d41b718c8c7f5647221d751c10430d3339429541
Rudhra Raveendran <rudytheninja@gmail.com>
2024-01-02 18:08:53 -0500

    Added basic checking logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,30 @@
 
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Playlist, SpotifyApi, UserProfile } from '@spotify/web-api-ts-sdk';
+import { Playlist, PlaylistedTrack, SpotifyApi, UserProfile } from '@spotify/web-api-ts-sdk';
 import { useEffect, useState } from 'react';
+import Fuse, { FuseResult } from 'fuse.js'
 
 // Choose one of the following:
 const sdk = SpotifyApi.withUserAuthorization(process.env.SPOTIFY_CLIENT_ID || "", process.env.REDIRECT_TARGET || "", ["playlist-read-private", "playlist-read-collaborative"]);
 
 /* stuff to do
-- make login button/flow so its not automatic
-- have playlists show name beneath image
-- have search for playlists
-- be able to select target playlist
-- be able to select
+- pass full playlist object through to comparison so that we can pull name as needed
+- display result cleanly
+- clean up code
 */
 
 export default function Home() {
 
   const [user, setUser] = useState<UserProfile | undefined>(undefined);
   const [playlists, setPlaylists] = useState<Playlist[]>([]);
+  const [filteredPlaylists, setFilteredPlaylists] = useState<Playlist[]>([]);
+  const [filterText, setFilterText] = useState("");
+
+  const [targetPlaylistId, setTargetPlaylistId] = useState("");
+  const [playlistsToCheck, setPlaylistsToCheck] = useState<string[]>([]);
+  const [phase, setPhase] = useState("target");
+
 
   useEffect(() => {
     const test = async () => {
@@ -42,98 +48,154 @@ export default function Home() {
     test()
   }, [])
 
+  useEffect(() => {
+    const search = (term: string): FuseResult<Playlist>[] => {
+      if (!term) {
+        // Just mimic the return type of `Fuse.FuseResult`
+        return playlists.map((item, refIndex) => ({
+          item,
+          refIndex,
+          matches: [],
+          score: 0,
+        }))
+      }
+
+      return fuse.search(term);
+    }
+
+
+    const fuse = new Fuse(playlists, { keys: ["name"] });
+    setFilteredPlaylists(search(filterText).map(fuseresult => fuseresult.item))
+  }, [playlists, filterText])
+
+  const handlePlaylistClick = (playlistId: string) => {
+    switch (phase) {
+      case "target":
+        setTargetPlaylistId(playlistId);
+        setPhase("check");
+        break;
+      case "check":
+        if (playlistsToCheck.includes(playlistId)) {
+          setPlaylistsToCheck(playlistsToCheck.filter(id => id !== playlistId));
+        } else {
+          setPlaylistsToCheck([...playlistsToCheck, playlistId]);
+        }
+        break;
+      default:
+        alert("how did u get here?");
+    }
+  }
+
+  const getPlaylistTracks = async (playlistId: string) => {
+    const tracks: PlaylistedTrack[] = [];
+    let res = await sdk.playlists.getPlaylistItems(playlistId, undefined, undefined, 50);
+    tracks.push(...res.items);
+    while (res.next) {
+      res = await sdk.playlists.getPlaylistItems(playlistId, undefined, undefined, 50, 50 + res.offset);
+      tracks.push(...res.items);
+    }
+    return tracks;
+  }
+
+  const comparePlaylists = async () => {
+    if (targetPlaylistId === "") {
+      alert("no target!")
+      return;
+    }
+
+    if (playlistsToCheck.length === 0) {
+      alert("no playlists to check!");
+      return;
+    }
+
+    // final datastructure should be {id, playlists: []}, if playlists is 1, we good
+    type ResultValue = {
+      track: PlaylistedTrack,
+      playlists: string[]
+    }
+    interface Map {
+      [key: string]: ResultValue | undefined
+    }
+
+    console.log("getting target tracks")
+    const targetTracks = await getPlaylistTracks(targetPlaylistId);
+    console.log(`got ${targetTracks.length} tracks`)
+    const result: Map = {}
+    targetTracks.forEach(track => {
+      result[track.track.id] = {
+        track: track,
+        playlists: [targetPlaylistId]
+      }
+    })
+    console.log("checking against playlists")
+    for (const playlist of playlistsToCheck) {
+      console.log(`getting tracks for ${playlist}`)
+      const tracksToCheck = await getPlaylistTracks(playlist);
+      console.log(`got ${tracksToCheck.length} tracks to check`)
+      for (const track of tracksToCheck) {
+        if (result[track.track.id] !== undefined) {
+          console.log("got a hit, adding playlist to result")
+          result[track.track.id]?.playlists.push(playlist);
+        }
+      }
+    }
+
+    console.log(result)
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
           Hello {user?.display_name}!
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      </div>
+
+
+      <div className={styles.grid}>
+        <h2
+          className={styles.card + " " + (phase === "target" ? styles.selected_phase : "")}
+          onClick={() => setPhase("target")}
+        >
+          Select Target <span>-&gt;</span>
+        </h2>
+
+        <h2
+          className={styles.card + " " + (phase === "check" ? styles.selected_phase : "")}
+          onClick={() => setPhase("check")}
+        >
+          Select Playlists to Check <span>-&gt;</span>
+        </h2>
+
+        <h2
+          className={styles.card}
+          onClick={comparePlaylists}
+        >
+          Run <span>-&gt;</span>
+        </h2>
+      </div>
+
+      <div>
+        Search: <input type="text" value={filterText} onChange={(e => setFilterText(e.target.value))} />
       </div>
 
       <div className={styles.center} id={styles.playlists}>
-        {playlists.map(playlist => (
-          <div>
-            <Image
-              className={styles.logo}
-              src={playlist.images[0].url}
-              alt={`${playlist.name} cover image`}
-              width={300}
-              height={300}
-              priority
-            />
-            <p>{playlist.name}</p>
-          </div>
-
-        ))}
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {filteredPlaylists
+          .sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+          .map(playlist => (
+            <div key={playlist.id} onClick={() => handlePlaylistClick(playlist.id)}
+              className={(targetPlaylistId === playlist.id ? styles.target : "") + " " + (playlistsToCheck.includes(playlist.id) ? styles.check : "")}>
+              <Image
+                className={styles.logo}
+                src={playlist.images[0].url}
+                alt={`${playlist.name} cover image`}
+                width={300}
+                height={300}
+                priority
+              />
+              <p>{playlist.name}</p>
+            </div>
+          ))}
       </div>
     </main>
   )
