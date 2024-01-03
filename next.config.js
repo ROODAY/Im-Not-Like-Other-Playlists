cb2f5c9aee839e122029215fe2a588ade8cfa757
Rudhra Raveendran <rudytheninja@gmail.com>
2024-01-02 23:23:14 -0500

    ready for publish

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
         SPOTIFY_CLIENT_ID: '775e8b1bd8f341fe97593357e4cdf480',
         REDIRECT_TARGET: 'http://localhost:3000'
     },
+    output: "export",
+    basePath: "/Im-Not-Like-Other-Playlists",
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: "https",
